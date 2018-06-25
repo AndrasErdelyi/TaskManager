1df73952e267afa1dd0afb7438520d9159e174ad
Andras <andris.erdelyi@gmail.com>
2018-06-25 22:34:26 +0200

    no message

--- Job/src/main/java/com/erdelyiandras/job/repository/RightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.erdelyiandras.job.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.erdelyiandras.job.entity.Right;
-
-public interface RightRepository extends CrudRepository<Right, Long>{
-
-}
